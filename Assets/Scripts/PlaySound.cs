3be9b89faa3ffea27ea00214233eae6c122bb093
Paulo Matos <paulojomatos@gmail.com>
2017-02-11 11:34:22 -0400

    first commit

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class PlaySound : ConditionBasedBehavior {
+
+    public AudioSource sound;
+    
+    public override void OnConditionSatisfied () {
+        sound.Play();
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(PlaySound))]
+public class PlaySoundEditor : ConditionBasedEditor {
+
+}
+#endif
